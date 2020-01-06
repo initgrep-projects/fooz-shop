413daa78e6ffba867647d86ee743c0887c83399c
igagrock <irshad.mike@gmail.com>
2020-01-07 01:12:36 +0530

    added inital ui design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,72 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemCouroselComponent } from './item-courosel/item-courosel.component';
+import { ItemsComponent } from './items/items.component';
+
+
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faUpload,
+  faDownload,
+  faChevronRight,
+  faChevronDown,
+  faChevronLeft,
+  faChevronCircleUp,
+  faList,
+  faSearch,
+  faFileWord,
+  faFilePdf,
+  faFileExcel,
+  faFilePowerpoint,
+  faFileAlt,
+  faEnvelope,
+  faCircleNotch,
+  faSpinner
+
+} from '@fortawesome/free-solid-svg-icons';
+import { TrendComponent } from './trend/trend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ItemCouroselComponent,
+    ItemsComponent,
+    TrendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(iconLiberary: FaIconLibrary) {
+    iconLiberary.addIcons(
+      faUpload,
+      faDownload,
+      faChevronRight,
+      faChevronDown,
+      faChevronLeft,
+      faChevronCircleUp,
+      faList,
+      faSearch,
+      faFileWord,
+      faFilePdf,
+      faFileExcel,
+      faFilePowerpoint,
+      faFileAlt,
+      faEnvelope,
+      faCircleNotch,
+      faSpinner
+    );
+  }
+
+ }
---
+++ src/app/item-courosel/item-courosel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-courosel',
+  templateUrl: './item-courosel.component.html',
+  styleUrls: ['./item-courosel.component.scss']
+})
+export class ItemCouroselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

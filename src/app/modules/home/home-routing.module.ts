784111266c3d33ed253fdcacb4faa703aa878cb9
irshad <irshad.mike@gmail.com>
2020-03-08 23:08:53 +0530

    added resolver to home featureItems component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { ItemDetailComponent } from '../shop/items/item-detail/item-detail.component';
+import { ProductResolver } from 'src/app/services/product-resolver.service';
 
 
 const homeRoutes: Routes  = [
   {path : 'home', component: HomeComponent},
-  {path: 'home/item/:id', component: ItemDetailComponent},
+  {path: 'home/item/:id', component: ItemDetailComponent , resolve: {product: ProductResolver}},
 ];
 
 @NgModule({

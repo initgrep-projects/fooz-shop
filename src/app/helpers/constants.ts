e0935cf102fdd24b3665696f67df71b0de724d30
irshad <irshad.mike@gmail.com>
2020-05-30 23:41:49 +0530

    added app constants file to hold all the constants at one place

---
+++ src/app/helpers/constants.ts
@@ -0,0 +1,5 @@
+export const ALERT_TITLE = 'alert';
+export const OK_BUTTON = 'ok';
+export const CANCEL_BUTTON = 'cancel';
+export const CART_ITEM_EXIST = 'A similar product already exists in your cart. Would u like to add again?';
+export const CART_ITEM_MAX_QUANTITY = 'You have added all the available products of this type.';

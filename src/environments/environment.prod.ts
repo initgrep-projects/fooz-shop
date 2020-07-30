dfa0011dd5fe50ccf26e53aedeea6894d7968a8e
irshad <irshad.mike@gmail.com>
2020-07-31 00:19:42 +0530

    added few changes for design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
+  firebaseConfig: {
     apiKey: 'AIzaSyCd-ji6MKq7zO6xkvZSq3ynGYOrAHltVY8',
     authDomain: 'foozabayas.firebaseapp.com',
     databaseURL: 'https://foozabayas.firebaseio.com',
@@ -10,14 +10,16 @@ export const environment = {
     appId: '1:226094433636:web:f5de3d568840beb15be671',
     measurementId: 'G-6H5QVBER9C'
   },
-  geoAddress :{
+  geoAddress: {
     apiToken: 'FvP2hbvX7qbpnyps6_nwJ21OvCRkEk3-KhRUGF4A_WpFOn4PAfSK1RHJljxc3faSnAY',
     accessTokenUrl: 'https://www.universal-tutorial.com/api/getaccesstoken',
     userEmail: 'initgrep@gmail.com',
     countriesUrl: 'https://www.universal-tutorial.com/api/countries/',
     statesUrl: 'https://www.universal-tutorial.com/api/states/',
     cityUrl: 'https://www.universal-tutorial.com/api/cities/'
-
+  },
+  payment: {
+    clientTokenUrl: 'http://localhost:3100/foozabayas/us-central1/api/pay/client_token'
   }
 
 };
